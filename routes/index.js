627d99d7d27b1c1b6dff7023c24a5f81d9aba0c6
rodrigo.rubio@gmail.com <rodrigo.rubio@gmail.com>
2016-12-18 22:51:46 +1100

    Revert "Corrected code, wrong file versions committed."
    This reverts commit 187f5097062be2f8d9e279057d620238c4a11120.

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,14 @@ var emailModule = require('./emailModule');
 
 // sendgrid - primary provider
 const SENDGRID_URL      = 'https://api.sendgrid.com/v3/mail/send';
-const SENDGRID_USER     = '';
-const SENDGRID_API_KEY = 'Bearer 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';  
+const SENDGRID_API_KEY = '<YOUR_API_KEY>';  
 
 // mailgun - secondary provider (failover)
-const MAIL_GUN_URL     = 'https://api.mailgun.net/v3/sandboxa4d6f1777ca24f768187097a4d72ddf9.mailgun.org/messages';
+const MAIL_GUN_URL     = '<YOUR_GUNMAIL_DOMAIN_HERE>';
 const MAIL_GUN_USER    = 'api';
-const MAIL_GUN_API_KEY = 'key-31a6b59573fbce3322f016573f3abeb2';
+const MAIL_GUN_API_KEY = '<YOUR_API_KEY>';
 
-const FROM_EMAIL       = 'rodrigo.rubio@gmail.com';  
+const FROM_EMAIL       = '<YOUR_FROM_EMAIL>';  
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,11 +22,11 @@ router.get('/', function(req, res, next) {
 
 router.post('/testMailGunPost', function(req, res, next){
 
-  var subject      = req.body['emailSubject'];
-  var emailMessage = req.body['emailMessage'];
-  var to  = emailModule.mailGunList(req.body['toEmail']);
-  var cc  = emailModule.mailGunList(req.body['ccEmail']);
-  var bcc = emailModule.mailGunList(req.body['bccEmail']);
+  var subject      = JSON.stringify(req.body.emailSubject);
+  var emailMessage = JSON.stringify(req.body.emailMessage);
+  var to  = emailModule.mailGunList(JSON.stringify(req.body.toEmail));
+  var cc  = emailModule.mailGunList(JSON.stringify(req.body.ccEmail));
+  var bcc = emailModule.mailGunList(JSON.stringify(req.body.bccEmail));
 
   var options = {
     "method": "POST",
@@ -56,11 +55,11 @@ router.post('/testMailGunPost', function(req, res, next){
 router.post('/testPostSendgrid', function(req, res, next) {
 
 var emails = {};
-emails['subject']  = req.body['emailSubject'];
-emails['emailMessage'] = req.body['emailMessage'];
-emails['recipients'] = emailModule.sendGridList(req.body['toEmail'], req.body['ccEmail'], req.body['bccEmail']);
-
-console.log(emails['recipients']);
+emails['subject']  = JSON.stringify(req.body.emailSubject);
+emails['emailMessage'] = JSON.stringify(req.body.emailMessage);
+emails['toEmail']  = emailModule.sendGridList(JSON.stringify(req.body.toEmail));
+emails['ccEmail']  = emailModule.sendGridList(JSON.stringify(req.body.ccEmail));
+emails['bccEmail'] = emailModule.sendGridList(JSON.stringify(req.body.bccEmail));
 
 var options = { method: 'POST',
   url: SENDGRID_URL,
@@ -69,23 +68,41 @@ var options = { method: 'POST',
      'content-type': 'application/json',
      authorization: SENDGRID_API_KEY 
   },
-  body:{ 
-    personalizations: emails['recipients'],
+  body: 
+   { personalizations: [ { 
+     to: [ 
+       JSON.parse(emails['toEmail'])
+     ],
+     cc: [
+       JSON.parse(emails['ccEmail']) 
+     ],
+     bcc: [
+       JSON.parse(emails['bccEmail']) 
+     ]
+     } ],
      from: { email: FROM_EMAIL },
-     subject: emails['subject'],
-     content: [ { type: 'text/plain', value: emails['emailMessage'] } ] 
-    },
-    json: true 
-}; 
+     subject: JSON.parse(emails['subject']),
+     content: [ { type: 'text/plain', value: JSON.parse(emails['emailMessage']) } ] },
+  json: true }; 
+
+
+  console.log('OPTIONS :: ' + JSON.stringify(options));
 
   request(options, function(error, response, formContent) { 
     
+    console.log('*** ERROR *** ' + JSON.stringify(error));
+    console.log('*** RESPONSE *** ' + JSON.stringify(error));
+
     if(response.statusCode == 200 || response.statusCode == 202) {
-      res.status(response.statusCode).send({ 'status' : response.statusCode });    
+
+      res.status(response.statusCode).send({ 'status' : response.statusCode });
+    
     } else {
+      console.log('*** ERROR *** ' + JSON.stringify(error));
       //NOTE: Error expected here due to none unique email address
       //      to demonstrate failover. Only when cc & bcc are the same.      
       res.status(response.statusCode).send({ 'status' : response.statusCode });
+
     }
 
   });
